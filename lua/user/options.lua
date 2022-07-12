2a274558ea2cf62a8928f50bee687051020b6a44
thatanjan <thatanjan@gmail.com>
2022-07-12 12:25:07 +0600

    some indent options are added

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,7 @@
 local options = {
 	backup = false, -- creates a backup file
 	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-	cmdheight = 1, -- more space in the neovim command line for displaying messages
+	cmdheight = 2, -- more space in the neovim command line for displaying messages
 	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
 	conceallevel = 0, -- so that `` is visible in markdown files
 	fileencoding = "utf-8", -- the encoding written to a file
@@ -24,6 +24,9 @@ local options = {
 	expandtab = true, -- convert tabs to spaces
 	shiftwidth = 2, -- the number of spaces inserted for each indentation
 	tabstop = 2, -- insert 2 spaces for a tab
+	autoindent = true, -- autoindent
+	preserveindent = true, -- preserve indentation
+	copyindent = true, -- copy indentation
 	cursorline = true, -- highlight the current line
 	number = true, -- set numbered lines
 	laststatus = 3,
@@ -34,7 +37,8 @@ local options = {
 	wrap = false, -- display lines as one long line
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
-	guifont = "monospace:h17",               -- the font used in graphical neovim applications
+	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	relativenumber = true, -- set relative numbered lines
 }
 
 vim.opt.fillchars.eob = " "
