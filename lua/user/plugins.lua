b1191deb96e54749668ed149ce4b81026f14c7c3
thatanjan <thatanjan@gmail.com>
2022-07-14 23:29:52 +0600

    copilot is installed

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -101,6 +101,9 @@ return packer.startup(function(use)
 	-- Which Key
 	use({ "folke/which-key.nvim" })
 
+	-- Copilot
+	use({ "github/copilot.vim" })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
