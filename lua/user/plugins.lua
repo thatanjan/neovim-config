97967a389e52a1baabc95928a362be4ba7898041
thatanjan <thatanjan@gmail.com>
2022-07-15 00:48:15 +0600

    mdx plugin is installed

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -101,6 +101,9 @@ return packer.startup(function(use)
 	-- Copilot
 	use({ "github/copilot.vim" })
 
+	-- MDX
+	use("findango/vim-mdx")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
