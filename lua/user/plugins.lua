1516796fbdf2b3ef844dae50d6c77e72068338da
thatanjan <thatanjan@gmail.com>
2022-07-15 18:30:09 +0600

    emmet is installed

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
 		end,
 	})
 
+	-- Emmet
+	use("mattn/emmet-vim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
