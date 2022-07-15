40a8317b7140187e558da20cdf4f9e9ebe6c7e66
thatanjan <thatanjan@gmail.com>
2022-07-15 23:34:50 +0600

    telescope keymaps are removed

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -55,12 +55,6 @@ keymap("v", ">", ">gv", opts)
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
--- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
-keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
 
