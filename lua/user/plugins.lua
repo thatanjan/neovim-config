f5ab6d2fda3d7404615d88396650edfd89515d16
thatanjan <thatanjan@gmail.com>
2022-07-18 12:54:06 +0600

    nvim-ts-rainbow plugin is installed for treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -87,6 +87,9 @@ return packer.startup(function(use)
 	-- Treesitter
 	use({ "nvim-treesitter/nvim-treesitter" })
 
+	-- Treesitter Plugins
+	use({ "p00f/nvim-ts-rainbow" })
+
 	-- Git
 	use({ "lewis6991/gitsigns.nvim" })
 
