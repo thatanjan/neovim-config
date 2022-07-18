565ce8299dd7a4b8462bb716e19a80efb0c419cd
thatanjan <thatanjan@gmail.com>
2022-07-18 17:09:30 +0600

    unnecessary print statement is removed

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -28,7 +28,6 @@ end
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
 	if client.supports_method("textDocument/formatting") then
-		print(client.name)
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			group = augroup,
