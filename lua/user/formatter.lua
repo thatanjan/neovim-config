44b264cc76886b3c3eb4d75c23f13e9d959cd28f
Anjan Shomodder <thatanjan@gmail.com>
2022-09-15 16:05:56 +0600

    Add formatter for less

--- lua/user/formatter.lua
+++ lua/user/formatter.lua
@@ -82,6 +82,16 @@ formatter.setup({
 		css = {
 			require("formatter.filetypes.css").prettier,
 		},
+		less = {
+			require("formatter.filetypes.css").prettier,
+			function()
+				return {
+					exe = "prettier",
+					args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
+					stdin = true,
+				}
+			end,
+		},
 		javascript = prettierConfig,
 		javascriptreact = prettierConfig,
 		json = {
