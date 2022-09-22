5a9b1c38528312793c3c199755bda30c6efa0ff9
Anjan Shomodder <thatanjan@gmail.com>
2022-09-22 11:53:21 +0600

    ;; will make any word jsx not enter

--- lua/user/emmet.lua
+++ lua/user/emmet.lua
@@ -2,5 +2,5 @@ vim.g.user_emmet_mode = "i"
 vim.g.user_emmet_leader_key = ","
 
 vim.cmd([[
-  nnoremap <silent><CR> :call emmet#expandAbbr(0,"")<cr>h:call emmet#splitJoinTag()<cr>ww
+  nnoremap <silent>;; :call emmet#expandAbbr(0,"")<cr>h:call emmet#splitJoinTag()<cr>ww
 ]])
