7cb9eee58bbec6d51ec4163f0aa2a609f6a66f0e
Anjan Shomodder <thatanjan@gmail.com>
2023-02-24 16:28:59 +0600

    Make indtents colorful

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -3,6 +3,16 @@ if not status_ok then
     return
 end
 
+vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.opt.list = true
+
 indent_blankline.setup {
     char = "▏",
     show_trailing_blankline_indent = false,
@@ -15,5 +25,13 @@ indent_blankline.setup {
         "help",
         "packer",
         "NvimTree",
+    },
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
     },
 }
