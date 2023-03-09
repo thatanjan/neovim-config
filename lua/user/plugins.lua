12d46ce62957961a54323da41764e5ad03f7176c
Anjan Shomodder <thatanjan@gmail.com>
2023-03-09 20:27:31 +0600

    Install vim transparent plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -168,6 +168,10 @@ return packer.startup(function(use)
         "norcalli/nvim-colorizer.lua",
     }
 
+    use {
+        "xiyaowong/nvim-transparent",
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
