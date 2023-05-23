ee711910f1b188b489478031763b548a2715646a
thatanjan <thatanjan@gmail.com>
2023-05-23 13:59:36 +0600

    Update comment.nvim config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -22,4 +22,8 @@ configs.setup {
         -- colors = {}, -- table of hex strings
         -- termcolors = {} -- table of colour name strings
     },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    },
 }
