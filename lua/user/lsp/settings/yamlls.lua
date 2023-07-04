effb13e72c72a9bef433ecda7991948b607ed1bb
Anjan Shomodder <thatanjan@gmail.com>
2023-07-04 12:26:29 +0600

    REmove schemastore from yamlls

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -4,12 +4,6 @@ return {
             format = {
                 enable = true,
             },
-            schemaStore = {
-                -- TODO: You must disable built-in schemaStore support if you want to use
-                -- this plugin and its advanced options like `ignore`.
-                enable = false,
-            },
-            schemas = require("schemastore").yaml.schemas(),
         },
     },
 }
