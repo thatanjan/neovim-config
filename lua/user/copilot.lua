bb546161691fb31ba3eee0dfbd777bb233b7d7d3
Anjan Shomodder <thatanjan@gmail.com>
2024-03-30 12:06:01 +0600

    Fix keymap customization

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -3,22 +3,14 @@ vim.g.copilot_filetypes = { xml = false, markdown = true, markdownjsx = true }
 
 -- since most are enabled by default you can turn them off
 -- using this table and only enable for a few filetypes
--- vim.g.copilot_filetypes = { ["*"] = false, python = true }
+vim.g.copilot_filetypes = { ["*"] = true, python = true }
 
-vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_no_tab_map = true
 vim.cmd [[imap <silent><script><expr> <A-j> copilot#Accept("\<CR>")]]
+vim.g.copilot_no_tab_map = true
 
--- vim.keymap.set("i", "<leader>j", "copilot#Accept('\\<CR>')<CR>", { silent = true })
-
--- <C-]>                   Dismiss the current suggestion.
--- <Plug>(copilot-dismiss)
---
---                                                 *copilot-i_ALT-]*
--- <M-]>                   Cycle to the next suggestion, if one is available.
--- <Plug>(copilot-next)
---
---                                                 *copilot-i_ALT-[*
--- <M-[>                   Cycle to the previous suggestion.
--- <Plug>(copilot-previous)
+vim.keymap.set("i", "<C-\\>", "<Plug>(copilot-accept-word)", { silent = true })
 
 vim.cmd [[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
+
+vim.cmd [[let g:copilot_workspace_folders = ["~/git_projects/"]]
