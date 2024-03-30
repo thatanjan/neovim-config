9d3be68cf1d3e7763148e8e2892c292cca387aa9
Anjan Shomodder <thatanjan@gmail.com>
2024-03-30 12:12:59 +0600

    Update notify config

--- lua/user/notify.lua
+++ lua/user/notify.lua
@@ -1,9 +1,8 @@
 local notify = require "notify"
 
 local config = {
-    max_width = 100,
-    render = "wrapped-compact",
-    timeout=2000
+    max_width = 50,
+    timeout = 1000,
 }
 
 notify.setup(config)
