5ebfb04d759f0bcde2250689a42fe80642dcbfed
Anjan Shomodder <thatanjan@gmail.com>
2024-09-09 14:47:15 +0600

    Install tailwind tools

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -212,4 +212,17 @@ lazy.setup {
         opts = {},
         dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
     },
+
+    -- tailwind-tools.lua
+    {
+        "luckasRanarison/tailwind-tools.nvim",
+        name = "tailwind-tools",
+        build = ":UpdateRemotePlugins",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim", -- optional
+            "neovim/nvim-lspconfig", -- optional
+        },
+        opts = {}, -- your configuration
+    },
 }
