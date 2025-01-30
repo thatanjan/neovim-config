ec34f90f567dbd69eaae76bdd32993b685b85869
Anjan Shomodder <thatanjan@gmail.com>
2025-01-30 17:14:22 +0600

    Update copilot plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -202,11 +202,12 @@ lazy.setup {
     { "github/copilot.vim" },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
+        branch = "main",
         dependencies = {
             { "github/copilot.vim" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
+        build = "make tiktoken",
         opts = {
             debug = true, -- Enable debugging
             -- See Configuration section for rest
