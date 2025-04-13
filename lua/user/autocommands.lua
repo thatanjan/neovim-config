e985325bd2996b2d73897fbfd36d805021ae6a30
Anjan Shomodder <thatanjan@gmail.com>
2025-04-13 11:34:10 +0600

    fix: update highlight function call for text yank event

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -30,7 +30,7 @@ vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     callback = function()
-        vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+        vim.hl.on_yank { higroup = "Visual", timeout = 200 }
     end,
 })
 
