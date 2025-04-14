3e6abc27f2c95d7f50c2234525d219a017ac3f85
Anjan Shomodder <thatanjan@gmail.com>
2025-04-14 11:15:37 +0600

    fix: remove commented-out diagnostic sign definitions

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -17,17 +17,6 @@ M.capabilities.textDocument.foldingRange = {
 }
 
 M.setup = function()
-    -- local signs = {
-    --     { name = "DiagnosticSignError", text = "" },
-    --     { name = "DiagnosticSignWarn", text = "" },
-    --     { name = "DiagnosticSignHint", text = "" },
-    --     { name = "DiagnosticSignInfo", text = "" },
-    -- }
-    --
-    -- for _, sign in ipairs(signs) do
-    --     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    -- end
-
     local config = {
         virtual_text = false, -- disable virtual text
         signs = {
