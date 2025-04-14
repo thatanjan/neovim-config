ca0bfbdc3c4cd5ff0d05b10b435616daee48f53e
Anjan Shomodder <thatanjan@gmail.com>
2025-04-14 11:14:47 +0600

    Remove deprecated vim.fn.sign_define function

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -17,21 +17,38 @@ M.capabilities.textDocument.foldingRange = {
 }
 
 M.setup = function()
-    local signs = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
-    }
-
-    for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
+    -- local signs = {
+    --     { name = "DiagnosticSignError", text = "" },
+    --     { name = "DiagnosticSignWarn", text = "" },
+    --     { name = "DiagnosticSignHint", text = "" },
+    --     { name = "DiagnosticSignInfo", text = "" },
+    -- }
+    --
+    -- for _, sign in ipairs(signs) do
+    --     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    -- end
 
     local config = {
         virtual_text = false, -- disable virtual text
         signs = {
-            active = signs, -- show signs
+            text = {
+                [vim.diagnostic.severity.ERROR] = " ",
+                [vim.diagnostic.severity.WARN] = " ",
+                [vim.diagnostic.severity.INFO] = "󰋼 ",
+                [vim.diagnostic.severity.HINT] = "󰌵 ",
+            },
+            texthl = {
+                [vim.diagnostic.severity.ERROR] = "Error",
+                [vim.diagnostic.severity.WARN] = "Warning",
+                [vim.diagnostic.severity.HINT] = "Hint",
+                [vim.diagnostic.severity.INFO] = "Info",
+            },
+            numhl = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
+            },
         },
         update_in_insert = true,
         underline = true,
