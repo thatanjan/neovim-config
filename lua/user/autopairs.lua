28e2d4222a2ffc8efc315de8a44e1447ee7fa62c
Anjan Shomodder <thatanjan@gmail.com>
2025-06-20 23:30:49 +0600

    fix: remove nvim-cmp dependency from avante.lua and clean up formatting

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -26,10 +26,3 @@ npairs.setup {
     },
     map_cr = false,
 }
-
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-    return
-end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
