1fbae8206a40e7251cfc79546d79c4787dff8d41
Anjan Shomodder <thatanjan@gmail.com>
2025-06-20 23:37:58 +0600

    fix: clean up formatting and improve error handling in mason.lua

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,68 +1,70 @@
 local servers = {
-    "lua_ls",
-    "cssls",
-    "html",
-    "tsserver",
-    "pyright",
-    "bashls",
-    "jsonls",
-    "yamlls",
-    "bashls",
-    "emmet_language_server",
-    "phpactor",
+  "lua_ls",
+  "cssls",
+  "html",
+  "tsserver",
+  "pyright",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "bashls",
+  "emmet_language_server",
+  "phpactor",
 }
 
 local settings = {
-    ui = {
-        border = "none",
-        icons = {
-            package_installed = "◍",
-            package_pending = "◍",
-            package_uninstalled = "◍",
-        },
+  ui = {
+    border = "none",
+    icons = {
+      package_installed = "◍",
+      package_pending = "◍",
+      package_uninstalled = "◍",
     },
-    log_level = vim.log.levels.INFO,
-    max_concurrent_installers = 4,
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 local mason_lspconfig = require "mason-lspconfig"
 
 mason_lspconfig.setup {
-    ensure_installed = servers,
-    automatic_installation = true,
+  ensure_installed = servers,
+  automatic_installation = true,
 }
 
 local opts = {}
 
+local handler_status_ok, handlers = pcall(require, "user.lsp.handlers")
+
+if not handler_status_ok then
+  print "LSP handlers not found, please install lspconfig"
+  return
+end
+
 mason_lspconfig.setup_handlers {
 
-    function(server_name)
-        opts = {
-            on_attach = require("user.lsp.handlers").on_attach,
-            capabilities = require("user.lsp.handlers").capabilities,
-        }
+  function(server_name)
+    opts = {
+      on_attach = handlers.on_attach,
+      capabilities = handlers.capabilities,
+    }
 
-        local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server_name)
-        if require_ok then
-            opts = vim.tbl_deep_extend("force", conf_opts, opts)
-        end
+    if server_name == "tsserver" then
+      -- Use the typescript.nvim plugin
+      require("typescript").setup {
+        disable_commands = false, -- prevent the plugin from creating Vim commands
+        debug = false, -- enable debug logging for commands
+        go_to_source_definition = {
+          fallback = true, -- fall back to standard LSP definition on failure
+        },
+        server = { -- pass options to lspconfig's setup method
+          on_attach = opts.on_attach,
+        },
+      }
+      return false
+    end
 
-        if server_name == "tsserver" then
-            -- Use the typescript.nvim plugin
-            require("typescript").setup {
-                disable_commands = false, -- prevent the plugin from creating Vim commands
-                debug = false, -- enable debug logging for commands
-                go_to_source_definition = {
-                    fallback = true, -- fall back to standard LSP definition on failure
-                },
-                server = { -- pass options to lspconfig's setup method
-                    on_attach = opts.on_attach,
-                },
-            }
-            return false
-        end
-
-        require("lspconfig")[server_name].setup(opts)
-    end,
+    require("lspconfig")[server_name].setup(opts)
+  end,
 }
