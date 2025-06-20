525af12f4d0c99705db0f3011d6fa6282606d680
Anjan Shomodder <thatanjan@gmail.com>
2025-06-20 23:38:02 +0600

    fix: improve error handling and loading sequence in LSP initialization

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,7 +1,20 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-    return
+  print "LSP not found, please install lspconfig"
+  return
 end
 
-require "user.lsp.mason"
-require("user.lsp.handlers").setup()
+local handler_status_ok, handlers = pcall(require, "user.lsp.handlers")
+
+if not handler_status_ok then
+  print "LSP handlers not found, please install lspconfig"
+  return
+end
+
+local mason_status_ok, mason_config = pcall(require, "user.lsp.mason")
+if not mason_status_ok then
+  print "LSP mason not found, please install mason"
+  return
+end
+
+handlers.setup()
