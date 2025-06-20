b85f008bad80932557d86256d598b1d8e3fd16c4
Anjan Shomodder <thatanjan@gmail.com>
2025-06-20 23:37:48 +0600

    fix: update LSP handlers to use blink.cmp for capabilities

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -1,14 +1,17 @@
 local M = {}
 
-local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_cmp_ok then
+local blink_cmp_status_ok, blink = pcall(require, "blink.cmp")
+
+if not blink_cmp_status_ok then
+    print "LSP handlers not found, please install lspconfig"
     return
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
+M.capabilities = vim.tbl_deep_extend("force", M.capabilities, blink.get_lsp_capabilities({}, false))
+
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- For nvim-ufo plugin
 M.capabilities.textDocument.foldingRange = {
