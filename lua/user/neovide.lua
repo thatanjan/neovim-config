8e5bd7da4621e207a9c445b9290d63116b8ae442
Anjan Shomodder <thatanjan@gmail.com>
2025-06-21 10:45:52 +0600

    fix: correct neovide transparency variable to opacity

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_opacity = 0.8
     vim.g.neovide_cursor_animation_length = 0.04
     vim.o.guifont = "FiraCode Nerd Font:h13"
     vim.opt.linespace = 5
