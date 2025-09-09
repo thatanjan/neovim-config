a9bf6ae790d7fd784d1f48f9cc504528dcdb0060
Anjan Shomodder <thatanjan@gmail.com>
2025-09-09 22:59:59 +0600

    ✨ enhance: add key mappings for Neovide scale factor adjustments
    - Added key mappings for increasing, decreasing, and resetting Neovide scale factor

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -3,4 +3,18 @@ if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0.04
     vim.o.guifont = "FiraCode Nerd Font:h13"
     vim.opt.linespace = 5
+
+    vim.api.nvim_set_keymap(
+        "n",
+        "<C-+>",
+        ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+        { silent = true }
+    )
+    vim.api.nvim_set_keymap(
+        "n",
+        "<C-->",
+        ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+        { silent = true }
+    )
+    vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
 end
