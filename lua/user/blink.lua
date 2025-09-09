ff29ac9fee3d060741c57da5aefa9dda67c589c1
Anjan Shomodder <thatanjan@gmail.com>
2025-09-09 23:01:27 +0600

    ✨ update: enhance blink.cmp configuration for improved key mappings and organization
    - Added custom key mappings for navigation and snippet handling
    - Improved organization and formatting of the configuration options

--- lua/user/blink.lua
+++ lua/user/blink.lua
@@ -1,107 +1,128 @@
 local M = {
-  "saghen/blink.cmp",
-  -- optional: provides snippets for the snippet source
-  dependencies = { "rafamadriz/friendly-snippets" },
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = { "rafamadriz/friendly-snippets" },
 
-  -- use a release tag to download pre-built binaries
-  version = "1.*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = "default" },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
 
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
-    },
+        snippets = { preset = "luasnip" },
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = {
-      documentation = { auto_show = true },
-      menu = {
-        draw = {
-          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = {
+            preset = "default",
+            -- CUSTOM MAPPINGS TO OVERRIDE/EXTEND THE PRESET:
+            -- Replicating your nvim-cmp C-k and C-j for item navigation
+            ["<C-k>"] = { "select_prev", "fallback" },
+            ["<C-j>"] = { "select_next", "fallback" },
+
+            ["<CR>"] = { "accept", "fallback" },
+
+            --   -- Replicating your nvim-cmp C-b and C-f for documentation scrolling
+            --   ["<C-b>"] = { "scroll_documentation_up" },
+            --   ["<C-f>"] = { "scroll_documentation_down" },
+
+            --   -- The crucial Tab for selection, snippet expansion, and jumping
+            --   ["<Tab>"] = { "select_next", "expand_snippet", "jump_next_snippet", "fallback" },
+
+            --   -- The crucial Shift-Tab for selection and jumping backward
+            --   ["<S-Tab>"] = { "select_prev", "jump_prev_snippet", "fallback" },
         },
-        components = {
-          kind_icon = {
-            text = function(ctx)
-              local icon = ctx.kind_icon
-              if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                if dev_icon then
-                  icon = dev_icon
-                end
-              else
-                icon = require("lspkind").symbolic(ctx.kind, {
-                  mode = "symbol",
-                })
-              end
 
-              return icon .. ctx.icon_gap
-            end,
-
-            -- Optionally, use the highlight groups from nvim-web-devicons
-            -- You can also add the same function for `kind.highlight` if you want to
-            -- keep the highlight groups in sync with the icons.
-            highlight = function(ctx)
-              local hl = ctx.kind_hl
-              if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                if dev_icon then
-                  hl = dev_hl
-                end
-              end
-              return hl
-            end,
-          },
+        appearance = {
+            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = "mono",
         },
-      },
-    },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      providers = {
-        buffer = {
-          opts = {
-            -- filter to only "normal" buffers
-            get_bufnrs = function()
-              return vim.tbl_filter(function(bufnr)
-                return vim.bo[bufnr].buftype == ""
-              end, vim.api.nvim_list_bufs())
-            end,
-          },
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = {
+            documentation = { auto_show = true },
+            menu = {
+                draw = {
+                    columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+                },
+                components = {
+                    kind_icon = {
+                        text = function(ctx)
+                            local icon = ctx.kind_icon
+                            if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                                if dev_icon then
+                                    icon = dev_icon
+                                end
+                            else
+                                icon = require("lspkind").symbolic(ctx.kind, {
+                                    mode = "symbol",
+                                })
+                            end
+
+                            return icon .. ctx.icon_gap
+                        end,
+
+                        -- Optionally, use the highlight groups from nvim-web-devicons
+                        -- You can also add the same function for `kind.highlight` if you want to
+                        -- keep the highlight groups in sync with the icons.
+                        highlight = function(ctx)
+                            local hl = ctx.kind_hl
+                            if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                                if dev_icon then
+                                    hl = dev_hl
+                                end
+                            end
+                            return hl
+                        end,
+                    },
+                },
+            },
         },
-      },
-    },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-  },
-  opts_extend = { "sources.default" },
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            providers = {
+                buffer = {
+                    opts = {
+                        -- filter to only "normal" buffers
+                        get_bufnrs = function()
+                            return vim.tbl_filter(function(bufnr)
+                                return vim.bo[bufnr].buftype == ""
+                            end, vim.api.nvim_list_bufs())
+                        end,
+                    },
+                },
+            },
+        },
+
+        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
 }
 
 return M
